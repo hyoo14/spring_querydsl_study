f8dadf811e9c2e6ecb5aa81e4ee5ac95f4e28733
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-07-29 11:02:00 +0900

    Query DSL Chapter 1 완료(3강-7강)

---
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,44 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+	//	@PersistenceContext //자바 표준, 스프링 말고 다른 컨테이너 만약에 쓴다면
+	@Autowired// 스프링 최신에서는 이것도 됨
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h"); //이것도 되고
+		QHello qHello = QHello.hello; //이것도 됨
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
